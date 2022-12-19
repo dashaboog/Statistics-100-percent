42bdac33f5cb5063f1ce831008423cc9b1e605b3
Neto study <daria.bugaevskaya@gmail.com>
2022-12-19 14:48:48 +0300

    Tests added

---
+++ src/test/java/ru/netology/ci/StatisticsServiceTest.java
@@ -0,0 +1,29 @@
+package ru.netology.ci;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StatisticsServiceTest {
+
+    @Test
+    void findMax() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxIfEquals() {
+        StatisticsService service = new StatisticsService();
+        long[] incomesInBillions = {11, 10, 5, 8, 4, 5, 3, 8, 6, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
